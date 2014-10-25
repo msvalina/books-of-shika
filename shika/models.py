397cd2804d9a95de39b9ec6df59c8464ec6f686b
Marijan Svalina <marijan.svalina@gmail.com>
2014-10-25 14:08:22 +0200

    models.py > first version of db model

--- shika/models.py
+++ shika/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    """ Book model """
+    name = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    is_lended = models.BoolenField(default=False)
+
+class BookOwner(model.Model):
+    """ Book owners model """
+    name = models.CharField(max_length=200)
+    book = models.ForeignKey(Book)
+
+class Reader(models.Model):
+    """ Reader model """
+    name = models.CharField(max_length=200, null=True, blank=True)
+    book = models.ForeignKey(Book)
+
+class LendingRequest(model.Model):
+    """ Book lending request """
+    reader = models.ForeignKey(Reader)
+    book_owner = models.ForeignKey(BookOwner)
+    book = models.ForeignKey(Book)
+    sent = models.BoolenField(default=False)
+    confirmed = models.BoolenField(default=False)
+    
+class LendingRecords(model.Model):
+    """ Book lending records model """ 
+    book = models.ForeignKey(Book)
+    book_owner = models.ForeignKey(BookOwner)
+    reader = models.ForeignKey(Reader)
+    request = LendingRequest(LendingRequest)
