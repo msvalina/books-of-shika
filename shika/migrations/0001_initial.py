58f43b24fd44c9ffa0467acc34f9b88567fa17a3
Marijan Svalina <marijan.svalina@gmail.com>
2014-10-28 07:41:42 +0100

    first db model migration

---
+++ shika/migrations/0001_initial.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Book',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('author', models.CharField(max_length=200)),
+                ('is_lended', models.BooleanField(default=False)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='BookOwner',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('book', models.ForeignKey(to='shika.Book')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='LendingRecords',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('book', models.ForeignKey(to='shika.Book')),
+                ('book_owner', models.ForeignKey(to='shika.BookOwner')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='LendingRequest',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('is_sent', models.BooleanField(default=False)),
+                ('is_confirmed', models.BooleanField(default=False)),
+                ('book', models.ForeignKey(to='shika.Book')),
+                ('book_owner', models.ForeignKey(to='shika.BookOwner')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Reader',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200, null=True, blank=True)),
+                ('book', models.ForeignKey(to='shika.Book')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='lendingrequest',
+            name='reader',
+            field=models.ForeignKey(to='shika.Reader'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='lendingrecords',
+            name='reader',
+            field=models.ForeignKey(to='shika.Reader'),
+            preserve_default=True,
+        ),
+    ]
