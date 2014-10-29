eb3ff7ccad5b3221cbf12a63f471c924bdb631b8
Marijan Svalina <marijan.svalina@gmail.com>
2014-10-29 07:30:21 +0100

    added bootstrap and debug_toolbar

--- books_of_shika/settings.py
+++ books_of_shika/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
@@ -36,7 +35,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shika'
+    'debug_toolbar',
+    'bootstrap3',
+    'shika',
 )
 
 MIDDLEWARE_CLASSES = (
