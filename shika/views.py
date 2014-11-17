d7534963e69855a1e61eb1f41d0b31ce969560ef
Marijan Svalina <marijan.svalina@gmail.com>
2014-11-17 07:58:02 +0100

    added more descriptive comments to views.py

--- shika/views.py
+++ shika/views.py
@@ -11,14 +11,21 @@ from shika.forms import BookEntryForm, LendingRequestForm
 
 @login_required
 def home(request):
+    """ Home view presenting number of unconfirmed requests and general info """
+
+    # Query set for unconfirmed requests, observe how query set can filter by
+    # other table/model row/attribute bc of foregin key relation
     unconfirmed_requests = LendingRequest.objects.filter(
                            book_owner__name=request.user,
                            is_confirmed=None).count()
     context = {'unconfirmed_requests_num': unconfirmed_requests}
+
     return render(request, 'shika/home.html', context)
 
 @login_required
 def collection(request):
+    """ Collection view presenting current users book collection """
+
     user = request.user.username
     books = Book.objects.filter(bookowner__name__username=user)
 
@@ -27,24 +34,25 @@ def collection(request):
     return render(request, 'shika/collection.html', context)
 
 def allcollections(request):
+    """ All collection view presenting all books in model/libraries """
+
     books = Book.objects.all()
 
     context = {'books': books}
 
+    # context_instance allows to use request object in template
     return render_to_response('shika/collection.html', context,
             context_instance=RequestContext(request))
 
 @login_required
 def book_entry(request):
-    # if this is a POST request we need to process the form data
+    """ View for adding books via form and POST method """
+
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = BookEntryForm(request.POST)
-        # check whether it's valid:
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
+            # process the data 
             book_entry = form.save(commit=False)
             book_entry.save()
             book_owner = BookOwner(name=request.user, book=book_entry)
@@ -52,7 +60,7 @@ def book_entry(request):
             messages.add_message(request, messages.INFO, 'Book added!')
             return redirect('/shika/bookentry/')
 
-    # if a GET (or any other method) we'll create a blank form
+    # if a GET (or any other method) create a blank form
     else:
         form = BookEntryForm()
 
@@ -60,68 +68,96 @@ def book_entry(request):
 
 @login_required
 def book_detail(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
+    """ Detail book view """
+
+    book = Book.objects.get(id=book_id)
+
     context = {'book': book}
 
     return render(request, 'shika/bookdetail.html', context)
 
 @login_required
 def lending_request(request):
-    # if this is a POST request we need to process the form data
+    """ Lending request view """
+
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = LendingRequestForm(request.POST)
-        # check whether it's valid:
         if form.is_valid():
-            # process the data in form.cleaned_data as required
+            # process the data 
             lending_req = form.save(commit=False)
             lending_req.is_sent = True
             book_owner = BookOwner.objects.get(book=lending_req.book)
             lending_req.book_owner = book_owner
             lending_req.save()
-            messages.add_message(request, messages.INFO, 'Request sent to %s!' % book_owner.name)
-            # redirect to a new URL:
+            messages.add_message(request, messages.INFO, 'Request sent to %s!' %
+                                 book_owner.name)
+
             return redirect('/shika/lending/')
 
-    # if a GET (or any other method) we'll create a blank form with inital value
-    # for reader input
+    # if a GET (or any other method) create a blank 
     else:
+        # Set initial value for LendingRequest form
         form = LendingRequestForm(initial={'reader': request.user})
-        form.fields['book'].queryset = Book.objects.exclude(bookowner__name=request.user)
-        form.fields['reader'].queryset = User.objects.filter(username=request.user.username)
+        # Set query sets for presenting on LendingRequest form fileds 
+        # Only show books that are not owned by logged in user
+        form.fields['book'].queryset = Book.objects.exclude(
+                                       bookowner__name=request.user)
+        # Set reader to logged in user
+        form.fields['reader'].queryset = User.objects.filter(
+                                         username=request.user.username)
 
     return render(request, 'lending.html', {'form': form})
 
 @login_required
 def confirm_request(request):
+    """ Confirm request view shows unconfirmed request """
 
+    # Query set for unconfirmed requests, observe how query set can filter by
+    # other table/model row/attribute bc of foregin key relation
     unconfirmed_requests = LendingRequest.objects.filter(
                            book_owner__name=request.user,
                            is_confirmed=None)
 
     if request.method == 'POST':
         try:
+            # capture value (req_id) from key Confirm set by POST method
             req_id = request.POST['Confirm']
+            # get related lending request
             req = LendingRequest.objects.get(id=req_id)
+            # change is_confirmed attribute
             req.is_confirmed = True
+            # save object to db
             req.save()
+            # create LendingRecord object from saved lending request
             record = LendingRecord(book=req.book, book_owner=req.book_owner,
-                    reader=req.reader, request=req)
+                     reader=req.reader, request=req)
+            # save record to db
             record.save()
+            # print message to the user
             messages.add_message(request, messages.INFO, 'Request confirmed!')
+
             return redirect('/shika/confirm/')
+
         except KeyError:
-            print 'WTf'
+            pass
+            # print 'WTf'
         try:
+            # get value from Deny key set in POST method
             req_id = request.POST['Deny']
-            print req_id
+            # get related lending request
             req = LendingRequest.objects.get(id=req_id)
+            # set is_confirmed to false and save object
             req.is_confirmed = False
             req.save()
-            messages.add_message(request, messages.INFO, 'Request denyed!')
+            # print message to the user
+            messages.add_message(request, messages.INFO, 'Request denied!')
+
             return redirect('/shika/confirm/')
+
         except KeyError:
-            print 'WTffff'
+            pass
+            # print 'WTffff'
 
     # if a GET (or any other method) we'll create a blank form
     else:
@@ -131,6 +167,8 @@ def confirm_request(request):
 
 @login_required
 def lending_records(request):
+    """ Lending records view shows current users lended books """
+
     records = LendingRecord.objects.filter(reader=request.user)
 
     context = {'records': records}
@@ -138,6 +176,7 @@ def lending_records(request):
     return render(request, 'shika/records.html', context)
 
 def logout_view(request):
+    """ Logout view called from books_of_shika/urls """
     logout(request)
     return redirect('/')
 
